36c58a2664298a181f3fc96a1fc98e8255d5f8cb
Andres Vejar <andresvejar@neubox.net>
2020-10-23 12:21:01 -0500

    fixed docker image base, update entrypoint for composer install/update componentes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
 FROM fakereto/nginx-fpm
+ENV PATH="/composer/vendor/bin:$PATH" \
+    COMPOSER_ALLOW_SUPERUSER=1
+
+RUN composer global require hirak/prestissimo
 COPY configs/app.conf ${NGINX_CONF_DIR}/sites-enabled/app.conf
 COPY entrypoint.sh /var/www/
+WORKDIR /var/www/app
+COPY --chown=www-data:www-data ./src .
+
+EXPOSE 80 443
 ENTRYPOINT ["/var/www/entrypoint.sh"]
+CMD ["/usr/bin/supervisord", "-n", "-c", "/etc/supervisor/supervisord.conf"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,50 @@
-#!/usr/bin/env bash
-composer install
-php artisan migrate
-php artisan db:seed
-set -e
-sed -i "s~ENV_SERVER_NAME~${ENV_SERVER_NAME}~g" ${NGINX_CONF_DIR}/sites-enabled/app.conf
-/usr/bin/supervisord
+#!/bin/bash
+set -eo pipefail
+shopt -s nullglob
+
+# Docker general functions for use on docker entrypoint files
+
+# logging functions
+docker_log() {
+	local type="$1"; shift
+	printf '%s [%s] [Entrypoint]: %s\n' "$(date --rfc-3339=seconds)" "$type" "$*"
+}
+
+# logging a note
+log_note() {
+	docker_log Note "$@"
+}
+# logging a warning
+log_warn() {
+	docker_log Warn "$@" >&2
+}
+# logging a error
+log_error() {
+	docker_log ERROR "$@" >&2
+	exit 1
+}
+
+# check to see if this file is being run or sourced from another script
+_is_sourced() {
+        # https://unix.stackexchange.com/a/215279
+        [ "${#FUNCNAME[@]}" -ge 2 ] \
+                && [ "${FUNCNAME[0]}" = '_is_sourced' ] \
+                && [ "${FUNCNAME[1]}" = 'source' ]
+}
+
+_main() {
+    sed -i "s~ENV_SERVER_NAME~${ENV_SERVER_NAME}~g" ${NGINX_CONF_DIR}/sites-enabled/app.conf
+    #Run base phpfpm image entrypoint
+    if [[ ! -f ./vendor ]]; then
+        composer install --prefer-dist --no-scripts --optimize-autoloader
+    else
+        composer update
+    fi
+    #php artisan migrate
+    exec "$@"
+}
+
+# If we are sourced from elsewhere, don't perform any further actions
+if ! _is_sourced; then
+    _main "$@"
+fi
